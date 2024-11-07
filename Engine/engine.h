5c117a35383c6bd618360c7b5da64c45c0a0ffc6
Maxim <m.noltmeer@gmail.com>
2024-11-07 16:03:35 +0200

    added ELI functions

--- Engine/engine.h
+++ Engine/engine.h
@@ -124,6 +124,45 @@ class KobzarEngine: public KE_INTERFACE
 };
 //-------------------------------------------------------------------------------
 
+ //функції-обгортки ELI для функціоналу рушія
+ void __stdcall eGetLastError(void *p);
+ void __stdcall eCreateStory(void *p);
+ void __stdcall eLoadStory(void *p);
+ void __stdcall eSaveStory(void *p);
+ void __stdcall eCloseStory(void *p);
+ void __stdcall eClearStory(void *p);
+ void __stdcall eAddScene(void *p);
+ void __stdcall eAddAnswer(void *p);
+ void __stdcall eAddScript(void *p);
+ void __stdcall eSelect(void *p);
+ void __stdcall eRemove(void *p);
+ void __stdcall eLink(void *p);
+ void __stdcall eUnlink(void *p);
+ void __stdcall eGetID(void *p);
+ void __stdcall eSetID(void *p);
+ void __stdcall eGetDialog(void *p);
+ void __stdcall eSetDialog(void *p);
+ void __stdcall eGetNextDialog(void *p);
+ void __stdcall eSetNextDialog(void *p);
+ void __stdcall eGetPrevID(void *p);
+ void __stdcall eSetPrevID(void *p);
+ void __stdcall eGetNextID(void *p);
+ void __stdcall eSetNextID(void *p);
+ void __stdcall eGetType(void *p);
+ void __stdcall eGetText(void *p);
  void __stdcall eSetText(void *p);
+ void __stdcall eIsEndDialog(void *p);
+ void __stdcall eSetEndDialog(void *p);
+ void __stdcall eGetParams(void *p);
+ void __stdcall eSetParams(void *p);
+ void __stdcall eRun(void *p);
+ void __stdcall eGetResult(void *p);
+
+ void __stdcall eTellStory(void *p);
+ void __stdcall eLoadDialog(void *p);
+ void __stdcall eGetScene(void *p);
+ void __stdcall eGetAnswerCount(void *p);
+ void __stdcall eGetAnswer(void *p);
+ void __stdcall eSelectAnswer(void *p);
 //-------------------------------------------------------------------------------
 #endif
